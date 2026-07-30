c2c19ea49a6463228bf4bbc6220296eadd8539e1
mraniruddhadas <dasaniruddha143@gmail.com>
2026-07-30 23:39:37 +0530

    Create script.js

---
+++ pages/script.js
@@ -0,0 +1,7 @@
+// ======================================
+// SME Finance Toolkit
+// Cash Flow Planner
+// Version 1.0
+// ======================================
+
+console.log("SME Finance Toolkit Loaded Successfully");
