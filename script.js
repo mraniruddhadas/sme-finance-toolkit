978d68f6e5abc5974bbb33a56a4cd443eb0e6bc3
mraniruddhadas <dasaniruddha143@gmail.com>
2026-07-30 23:54:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// SME Finance Toolkit Scripts
+// =====================================
+// SME Finance Toolkit
+// Cash Flow Planner
+// Version 1.0
+// =====================================
+
+console.log("SME Finance Toolkit Loaded Successfully");
+
+// Calculate Button
+
+const calculateButton = document.getElementById("calculate-btn");
+
+calculateButton.addEventListener("click", function () {
+
+    alert("Welcome to SME Finance Toolkit!");
+
+});
