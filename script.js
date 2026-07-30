082ee2e13a80bf196b43735e1661de1b0429c941
mraniruddhadas <dasaniruddha143@gmail.com>
2026-07-30 23:57:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-// =====================================
+// ======================================
 // SME Finance Toolkit
 // Cash Flow Planner
 // Version 1.0
-// =====================================
+// ======================================
 
 console.log("SME Finance Toolkit Loaded Successfully");
 
+// ===========================
+// Get Buttons
+// ===========================
+
+const calculateBtn = document.getElementById("calculate-btn");
+const resetBtn = document.getElementById("reset-btn");
+
+// ===========================
+// Summary Fields
+// ===========================
+
+const totalInflow = document.getElementById("totalInflow");
+const totalOutflow = document.getElementById("totalOutflow");
+const netCashFlow = document.getElementById("netCashFlow");
+const closingBalance = document.getElementById("closingBalance");
+
+// ===========================
+// Opening Balance
+// ===========================
+
+const openingBalance = document.getElementById("openingBalance");
+
+// ===========================
 // Calculate Button
+// ===========================
 
-const calculateButton = document.getElementById("calculate-btn");
+calculateBtn.addEventListener("click", calculateCashFlow);
 
-calculateButton.addEventListener("click", function () {
+// ===========================
+// Reset Button
+// ===========================
 
-    alert("Welcome to SME Finance Toolkit!");
+resetBtn.addEventListener("click", function () {
+
+    alert("Reset function coming soon.");
 
 });
+
+// ===========================
+// Main Function
+// ===========================
+
+function calculateCashFlow(){
+
+    alert("JavaScript is Connected Successfully!");
+
+}
